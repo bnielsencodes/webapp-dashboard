f72507b5e4a7d6a3a23b38e7368824249ad4d28b
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2022-06-12 20:31:22 -0700

    add charts

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 const alertBanner = document.getElementById("alert");
+const trafficCanvas = document.getElementById("traffic-chart");
+const dailyCanvas = document.getElementById("daily-chart");
+const mobileCanvas = document.getElementById("mobile-chart");
 
+
+/* ------------- ALERT ---------------*/
 // create the html for the banner
 alertBanner.innerHTML =
   `
@@ -11,9 +16,129 @@ alertBanner.innerHTML =
   </div>
   `
 
+// close alert when click on x
 alertBanner.addEventListener('click', e => {
   const element = e.target;
   if (element.classList.contains("alert-banner-close")) {
     alertBanner.style.display = "none"
   }
 });
+
+/* ------------- TRAFFIC LINE CHART ---------------*/
+// trraffic chart data
+let trafficData = {
+  labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3",
+    "4-10", "11-17", "18-24", "25-31"
+  ],
+  datasets: [{
+    data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500,
+      2500
+    ],
+    backgroundColor: 'rgba(116, 119, 191, .3)',
+    borderWidth: 1,
+  }]
+};
+
+// traffic chart options
+let trafficOptions = {
+  backgroundColor: 'rgba(112, 104, 201, .5)',
+  fill: true,
+  aspectRatio: 2.5,
+  animation: {
+    duration: 0
+  },
+  scales: {
+    y: {
+      beginAtZero: true
+    }
+  },
+  plugins: {
+    legend: {
+      display: false
+    }
+  }
+};
+
+// create traffic chart
+let trafficChart = new Chart(trafficCanvas, {
+  type: 'line',
+  data: trafficData,
+  options: trafficOptions,
+});
+
+/* ------------- DAILY TRAFFIC BAR CHART ---------------*/
+
+// daily traffic bar chart data
+const dailyData = {
+  labels: ["S", "M", "T", "W", "T", "F", "S"],
+  datasets: [{
+    label: '# of Hits',
+    data: [75, 115, 175, 125, 225, 200, 100],
+    backgroundColor: '#7477BF',
+    borderWidth: 1
+  }]
+};
+
+// daily traffic bar chart options
+const dailyOptions = {
+  scales: {
+    y: {
+      beginAtZero: true
+    }
+  },
+  plugins: {
+    legend: {
+      display: false
+    }
+  }
+};
+
+// create daily traffic bar chart
+let dailyChart = new Chart(dailyCanvas, {
+  type: 'bar',
+  data: dailyData,
+  options: dailyOptions
+});
+
+/* ------------- MOBILE USERS DONUT CHART ---------------*/
+
+// mobile users donut chart data
+
+const mobileData = {
+  labels: ["Desktop", "Tablet", "Phones"],
+  datasets: [{
+    label: '# of Users',
+    data: [2000, 550, 500],
+    borderWidth: 0,
+    backgroundColor: [
+      '#7477BF',
+      '#78CF82',
+      '#51B6C8'
+    ]
+  }]
+};
+
+// mobile users donut chart options
+
+const mobileOptions = {
+  aspectRatio: 1.9,
+  plugins: {
+    legend: {
+      position: 'right',
+      labels: {
+        boxWidth: 20,
+        fontStyle: 'bold'
+      }
+    }
+  }
+};
+
+// create mobile users donut chart
+
+let mobileChart = new Chart(mobileCanvas, {
+  type: 'doughnut',
+  data: mobileData,
+  options: mobileOptions
+});
+
+/* ------------- MESSAGING ---------------*/
